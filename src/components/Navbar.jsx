10b58f49a13034fc12d0f1260e461b73ffbab8fe
austint-code <austintan2006@gmail.com>
2025-08-31 21:33:18 -0400

    minor chnages

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -21,7 +21,7 @@ const Navbar = () => {
             }`}>
           About
         </Link>
-        <a href="https://drive.google.com/file/d/1kz0Oo3R3xNXYgFrj2YQTzkhr0g5iNlWs/view?usp=sharing" target="_blank" rel="noopener noreferrer" className="hover:text-purple-600 transition-colors text-lg font-semibold">
+        <a href="https://drive.google.com/file/d/14Y8jKr71trlgo99hE7JVZKXdsFyuQJUt/view?usp=sharing" target="_blank" rel="noopener noreferrer" className="hover:text-purple-600 transition-colors text-lg font-semibold">
           Resume
         </a>
       </div>
