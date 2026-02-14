18b67ccf9515b8e6b147bbcf63f4692bb99c47bd
austintan <austintan2006@gmail.com>
2026-02-13 20:53:41 -0500

    resume

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -30,7 +30,7 @@ const Navbar = () => {
           }`}></span>
         </Link>
         <a
-          href="https://drive.google.com/file/d/1yeFBS-_4CfDg1OPXFkhzk70P6XGkEzHA/view?usp=sharing"
+          href="https://drive.google.com/file/d/19gM4RyvFzhMOIbber1OPHr3oulv9jZf8/view?usp=sharing"
           target="_blank"
           rel="noopener noreferrer"
           className="text-lofi-brown hover:text-lofi-darkBrown transition-all duration-300 text-base font-light tracking-wide relative group">
